f178d3a1e36320ef671ec765cc612fa18ad0c1f0
Victor Hermes <me.victorhermes@gmail.com>
2020-05-10 19:55:33 -0300

    date-fns and uuid

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,15 +1,34 @@
 import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parsedDate, appointment.date),
+    );
+
+    if (findAppointmentInSameDate) {
+        response.status(400).json({ message: 'Esse horário já foi agendado' });
+    }
+
     const appointment = {
+        id: uuidv4(),
         provider,
-        date,
+        date: parsedDate,
     };
 
     appointments.push(appointment);
